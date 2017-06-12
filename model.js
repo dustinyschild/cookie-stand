2ae737f718c253e65e00ef7a17efcde4422e0cc2
Dustin <dyelland@gmail.com>
2017-06-12 17:47:27 -0500

    added in the .toFixed to random number generation

--- model.js
+++ model.js
@@ -7,119 +7,116 @@ var stores = [
     maxHourlyCustomers: 65,
     averagePurchasedPerSale: 6.3,
     customersPerHour: function() { //generate random customers
-      return this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
+      return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
     },
     purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
       return this.averagePurchasedPerSale * this.customersPerHour;
     },
     simulatedCookiesPerHour: function() {
-     var hours = [];
-     for (var i = 0; i < this.hoursOpen; i++) {
-       var customerCount = this.customersPerHour();
-       var time = (i + 6);
-       hours[i] = customerCount * this.averagePurchasedPerSale;
-       this.cookiesPerHour = hours;
-       console.log(time);
-       console.log(this.cookiesPerHour);
+      var hours = [];
+      for (var i = 0; i < this.hoursOpen; i++) {
+        var customerCount = this.customersPerHour();
+        var time = (i + 6);
+        hours[i] = customerCount * this.averagePurchasedPerSale;
+        this.cookiesPerHour = hours;
+        console.log(time);
+        console.log(this.cookiesPerHour);
       }
     },
   },
 
   SeaTacAirport = {
     hoursOpen: 14,
-    minHourlyCustomers: 3,
-    maxHourlyCustomers: 24,
-    averagePurchasedPerSale: 1.2,
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 65,
+    averagePurchasedPerSale: 6.3,
     customersPerHour: function() { //generate random customers
-      return this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
+      return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
     },
     purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
       return this.averagePurchasedPerSale * this.customersPerHour;
-    },//avg cookies purchasedPerHour * customersPerHour
+    },
     simulatedCookiesPerHour: function() {
-     var hours = [];
-     for (var i = 0; i < this.hoursOpen; i++) {
-       var customerCount = this.customersPerHour();
-       var time = (i + 6);
-       hours[i] = customerCount * this.averagePurchasedPerSale;
-       this.cookiesPerHour = hours;
-       console.log(time);
-       console.log(this.cookiesPerHour);
-     }
-    }
+      var hours = [];
+      for (var i = 0; i < this.hoursOpen; i++) {
+        var customerCount = this.customersPerHour();
+        var time = (i + 6);
+        hours[i] = customerCount * this.averagePurchasedPerSale;
+        this.cookiesPerHour = hours;
+        console.log(time);
+        console.log(this.cookiesPerHour);
+      }
+    },
   },
 
   SeattleCenter = {
     hoursOpen: 14,
-    minHourlyCustomers: 11,
-    maxHourlyCustomers: 38,
-    averagePurchasedPerSale: 3.7,
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 65,
+    averagePurchasedPerSale: 6.3,
     customersPerHour: function() { //generate random customers
-      return this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
+      return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
     },
     purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
       return this.averagePurchasedPerSale * this.customersPerHour;
-    },//avg cookies purchasedPerHour * customersPerHour
+    },
     simulatedCookiesPerHour: function() {
-     var hours = [];
-     for (var i = 0; i < this.hoursOpen; i++) {
-       var customerCount = this.customersPerHour();
-       var time = (i + 6);
-       hours[i] = customerCount * this.averagePurchasedPerSale;
-       this.cookiesPerHour = hours;
-       console.log(time);
-       console.log(this.cookiesPerHour);
-     }
-    }
+      var hours = [];
+      for (var i = 0; i < this.hoursOpen; i++) {
+        var customerCount = this.customersPerHour();
+        var time = (i + 6);
+        hours[i] = customerCount * this.averagePurchasedPerSale;
+        this.cookiesPerHour = hours;
+        console.log(time);
+        console.log(this.cookiesPerHour);
+      }
+    },
   },
 
   CapitolHill = {
     hoursOpen: 14,
-    minHourlyCustomers: 20,
-    maxHourlyCustomers: 38,
-    averagePurchasedPerSale: 2.3,
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 65,
+    averagePurchasedPerSale: 6.3,
     customersPerHour: function() { //generate random customers
-      return this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
+      return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
     },
     purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
       return this.averagePurchasedPerSale * this.customersPerHour;
-    },//avg cookies purchasedPerHour * customersPerHour
+    },
     simulatedCookiesPerHour: function() {
-     var hours = [];
-     for (var i = 0; i < this.hoursOpen; i++) {
-      var customerCount = this.customersPerHour();
-      var time = (i + 6);
-      hours[i] = customerCount * this.averagePurchasedPerSale;
-      this.cookiesPerHour = hours;
-      console.log(time);
-      console.log(this.cookiesPerHour);
-    }
-  }
-},
+      var hours = [];
+      for (var i = 0; i < this.hoursOpen; i++) {
+        var customerCount = this.customersPerHour();
+        var time = (i + 6);
+        hours[i] = customerCount * this.averagePurchasedPerSale;
+        this.cookiesPerHour = hours;
+        console.log(time);
+        console.log(this.cookiesPerHour);
+      }
+    },
+  },
 
   Alki = {
     hoursOpen: 14,
-    minHourlyCustomers: 2,
-    maxHourlyCustomers: 16,
-    averagePurchasedPerSale: 4.6,
-    customersPerHour: function() { //generate random customers
-      return this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers);
-    },
-    purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
-      return this.averagePurchasedPerSale * this.customersPerHour;
-    },//avg cookies purchasedPerHour * customersPerHour
-    simulatedCookiesPerHour: function() {
-     var hours = [];
-     for (var i = 0; i < this.hoursOpen; i++) {
-       var customerCount = this.customersPerHour();
-       var time = (i + 6);
-       hours[i] = customerCount * this.averagePurchasedPerSale;
-       this.cookiesPerHour = hours;
-       console.log(time);
-       console.log(this.cookiesPerHour);
-     }
-  }
-
-];
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     minHourlyCustomers: 23,
+     maxHourlyCustomers: 65,
+     averagePurchasedPerSale: 6.3,
+     customersPerHour: function() { //generate random customers
+       return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
+     },
+     purchasedPerHour: function() { //avg cookies purchasedPerSale * customersPerHour
+       return this.averagePurchasedPerSale * this.customersPerHour;
+     },
+     simulatedCookiesPerHour: function() {
+       var hours = [];
+       for (var i = 0; i < this.hoursOpen; i++) {
+         var customerCount = this.customersPerHour();
+         var time = (i + 6);
+         hours[i] = customerCount * this.averagePurchasedPerSale;
+         this.cookiesPerHour = hours;
+         console.log(time);
+         console.log(this.cookiesPerHour);
+       }
+     },
+   },
