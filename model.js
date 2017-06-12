02f7aadfff66ef2c2f237d545526c779f61091df
Dustin <dyelland@gmail.com>
2017-06-12 15:05:12 -0500

    added given data values for each store

--- model.js
+++ model.js
@@ -1,1 +1,54 @@
 //stores all information
+
+var stores = [
+  FirstAndPike,
+  SeaTacAirport,
+  SeattleCenter,
+  CapitolHill,
+  Alki
+]
+
+var FirstAndPike = {
+  minHourlyCustomers: 23
+  maxHourlyCustomers: 65
+  averagePurchasedPerSale: 6.3
+  customersPerHour: //generate random customers
+  purchasedPerHour: //avg cookies purchasedPerSale * customersPerHour
+  results: //
+}
+
+var SeaTacAirport = {
+  minHourlyCustomers: 3
+  maxHourlyCustomers: 24
+  averagePurchasedPerSale: 1.2
+  customersPerHour: //generate random
+  purchasedPerHour: //avg cookies purchasedPerHour * customersPerHour
+  results: //
+}
+
+var SeattleCenter = {
+  minHourlyCustomers: 11
+  maxHourlyCustomers: 38
+  averagePurchasedPerSale: 3.7
+  customersPerHour: //generate random
+  purchasedPerHour: //avg cookies purchasedPerHour * customersPerHour
+  results: //
+}
+
+var CapitolHill = {
+  minHourlyCustomers: 20
+  maxHourlyCustomers: 38
+  averagePurchasedPerSale: 2.3
+  customersPerHour: //generate random
+  purchasedPerHour: //avg cookies purchasedPerHour * customersPerHour
+  results: //
+}
+
+var Alki = {
+  minHourlyCustomers: 2
+  maxHourlyCustomers: 16
+  averagePurchasedPerSale: 4.6
+  customersPerHour: //generate random
+  purchasedPerHour: //avg cookies purchasedPerHour * customersPerHour
+  results: //
+}
