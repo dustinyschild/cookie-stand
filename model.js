53b42bca4f6f01737d51cb542e984c16381b0651
Dustin <dyelland@gmail.com>
2017-06-13 15:34:26 -0500

    added to AddStore prototype, outlined stores html doc, added some notes to index html doc

--- model.js
+++ model.js
@@ -1,69 +1,29 @@
 //stores all information
 
-  /*store1 = {
-    name: FirstAndPike,
-    hoursOpen: 14,//move to prototype
-    minHourlyCustomers: 23,
-    maxHourlyCustomers: 65,
-    averagePurchasedPerSale: 6.3,
-    customersPerHour: getRandomCustomers(),
-    purchasedPerHour: getPurchasedPerHour(),
-    simulatedCookiesPerHour: getSimulatedCookiesPerHour()
-  },
+var stores = [
+  new AddStore('FirstAndPike',23,65,6.3),
+  new AddStore('SeaTacAirport',3,24,1.2),
+  new AddStore('SeattleCenter',11,38,3.7),
+  new AddStore('CapitolHill',20,38,2.3),
+  new AddStore('Alki',2,16,4.6)
+];
 
-  store2 = {
-    name: SeaTacAirport,
-    hoursOpen: 14,
-    minHourlyCustomers: 3,
-    maxHourlyCustomers: 24
-    averagePurchasedPerSale: 1.2,
-    customersPerHour: getRandomCustomers(),
-    purchasedPerHour: getPurchasedPerHour(),
-    simulatedCookiesPerHour: getSimulatedCookiesPerHour(),
-  },
+//prototype values
+AddStore.prototype.hoursOpen = 15;
 
-  store3 = {
-    name: SeattleCenter,
-    hoursOpen: 14,
-    minHourlyCustomers: 11,
-    maxHourlyCustomers: 38,
-    averagePurchasedPerSale: 3.7,
-    customersPerHour: getRandomCustomers(),
-    purchasedPerHour: getPurchasedPerHour(),
-    simulatedCookiesPerHour: getSimulatedCookiesPerHour(),
-  },
+AddStore.prototype.getRandomCustomers = function() { //generate random customers
+  randomvalue = (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
+  console.log(this.minHourlyCustomers);
+  console.log(this.maxHourlyCustomers);
+  console.log(randomvalue);
+  return randomvalue;
+};
 
-  store4 = {
-    name: CapitolHill,
-    hoursOpen: 14,
-    minHourlyCustomers: 20,
-    maxHourlyCustomers: 38,
-    averagePurchasedPerSale: 2.3,
-    customersPerHour: getRandomCustomers(),
-    purchasedPerHour: getPurchasedPerHour(),
-    simulatedCookiesPerHour: getSimulatedCookiesPerHour(),
-  },
-
-  store5 = {
-    name: Alki,
-    hoursOpen: 14,
-    minHourlyCustomers: 2,
-    maxHourlyCustomers: 16,
-    averagePurchasedPerSale: 4.6,
-    customersPerHour: getRandomCustomers(),
-    purchasedPerHour: getPurchasedPerHour(),
-    simulatedCookiesPerHour: getSimulatedCookiesPerHour(),
-  },*/
-
-function getRandomCustomers() { //generate random customers
-  return (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
-}
-
-function getPurchasedPerHour() { //avg cookies purchasedPerSale * customersPerHour
+AddStore.prototype.getPurchasedPerHour = function() { //avg cookies purchasedPerSale * customersPerHour
   return this.averagePurchasedPerSale * this.customersPerHour;
-}
+};
 
-function getSimulatedCookiesPerHour() {
+AddStore.prototype.getSimulatedCookiesPerHour = function() {
   var hours = [];
   for (var i = 0; i < this.hoursOpen; i++) {
     var customerCount = this.customersPerHour();
@@ -73,7 +33,7 @@ function getSimulatedCookiesPerHour() {
     console.log(time);
     console.log(this.cookiesPerHour);
   }
-}
+};
 
 function AddStore(name,
   minHourlyCustomers,
@@ -81,24 +41,12 @@ function AddStore(name,
   averagePurchasedPerSale
 ) {
   this.name = name;
-  this.hoursOpen = stores.prototype.hoursOpen;
-  this.minHourlyCusomers = minHourlyCusomers;
+  this.minHourlyCusomers = minHourlyCustomers;
   this.maxHourlyCustomers = maxHourlyCustomers;
   this.averagePurchasedPerSale = averagePurchasedPerSale;
-  this.customersPerHour = getRandomCustomers();
-  this.purchasedPerHour = getPurchasedPerHour();
-  this.simulatedCookiesPerHour = getSimulatedCookiesPerHour();
+  this.customersPerHour = this.getRandomCustomers();
+  this.purchasedPerHour = this.getPurchasedPerHour();
+  this.simulatedCookiesPerHour = this.getSimulatedCookiesPerHour();
 }
-
-//prototype
-stores.prototype.hoursOpen = 15;
-
-var stores = [
-  new AddStore(FirstAndPike,23,65,6.3),
-  new AddStore(SeaTacAirport,3,24,1.2),
-  new AddStore(SeattleCenter,11,38,3.7),
-  new AddStore(CapitolHill,20,38,2.3),
-  new AddStore(Alki,2,16,4.6)
-];
 
 //make some functions using the DOM to add info to the document.
