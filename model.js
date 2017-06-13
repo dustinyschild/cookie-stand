fd5507cfc733c6d139275fec8f4489ee7c03b8f7
Dustin <dyelland@gmail.com>
2017-06-13 16:26:32 -0500

    got random customer, and purchased per customer functions in working order

--- model.js
+++ model.js
@@ -1,37 +1,27 @@
 //stores all information
 
-var stores = [
-  new AddStore('FirstAndPike',23,65,6.3),
-  new AddStore('SeaTacAirport',3,24,1.2),
-  new AddStore('SeattleCenter',11,38,3.7),
-  new AddStore('CapitolHill',20,38,2.3),
-  new AddStore('Alki',2,16,4.6)
-];
-
 //prototype values
 AddStore.prototype.hoursOpen = 15;
 
 AddStore.prototype.getRandomCustomers = function() { //generate random customers
-  randomvalue = (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
-  console.log(this.minHourlyCustomers);
-  console.log(this.maxHourlyCustomers);
-  console.log(randomvalue);
+  var randomvalue = (this.minHourlyCustomers + Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)).toFixed();
   return randomvalue;
 };
 
 AddStore.prototype.getPurchasedPerHour = function() { //avg cookies purchasedPerSale * customersPerHour
-  return this.averagePurchasedPerSale * this.customersPerHour;
+  console.log('Average Purchased: ' + this.averagePurchasedPerSale);
+  console.log('Customers Per Hour: ' + this.getRandomCustomers());
+  console.log(this.averagePurchasedPerSale);
+  return (this.averagePurchasedPerSale * this.getRandomCustomers());
 };
 
 AddStore.prototype.getSimulatedCookiesPerHour = function() {
   var hours = [];
   for (var i = 0; i < this.hoursOpen; i++) {
-    var customerCount = this.customersPerHour();
+    var customerCount = this.getRandomCustomers();
     var time = (i + 6);
     hours[i] = customerCount * this.averagePurchasedPerSale;
     this.cookiesPerHour = hours;
-    console.log(time);
-    console.log(this.cookiesPerHour);
   }
 };
 
@@ -41,12 +31,19 @@ function AddStore(name,
   averagePurchasedPerSale
 ) {
   this.name = name;
-  this.minHourlyCusomers = minHourlyCustomers;
+  this.minHourlyCustomers = minHourlyCustomers;
   this.maxHourlyCustomers = maxHourlyCustomers;
   this.averagePurchasedPerSale = averagePurchasedPerSale;
-  this.customersPerHour = this.getRandomCustomers();
   this.purchasedPerHour = this.getPurchasedPerHour();
   this.simulatedCookiesPerHour = this.getSimulatedCookiesPerHour();
 }
+
+var stores = [
+  new AddStore('FirstAndPike',23,65,6.3),
+  new AddStore('SeaTacAirport',3,24,1.2),
+  new AddStore('SeattleCenter',11,38,3.7),
+  new AddStore('CapitolHill',20,38,2.3),
+  new AddStore('Alki',2,16,4.6)
+];
 
 //make some functions using the DOM to add info to the document.
